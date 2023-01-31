5cf84e5a6fd7f5c8719d6373521508b09d513852
ThomasBaudon <tbaudon@yahoo.fr>
2023-01-31 16:15:08 +0100

    Trait slug + migrateur

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -7,10 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Trait\SlugTrait;
 
 #[ORM\Entity(repositoryClass: RoomRepository::class)]
 class Room
 {
+    use SlugTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
